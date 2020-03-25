2bd961704525033e0fb31ac63c2511efdfb40230
unknown <rubenpintogonzalez@gmail.com>
2020-03-25 01:45:39 -0300

    subiendo una partica

---
+++ practica.js
@@ -0,0 +1,13 @@
+if(1 > 2){
+    console.log('no');
+    
+    if(12 > 2){
+        console.log('si es mayor1');
+}    
+} else if(10 > 10 ){
+    console.log('si es mayor2');
+    
+} else if(12 > 10){
+    console.log('si es mayor node');
+    
+}
